ffc32b09f6de08a24b0a39297fe644ef856d5b5f
Adam Pugliese <adampugliese@Adams-MBP.attlocal.net>
2021-11-12 11:07:30 -0500

    wip

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'Django>=1.3',
         'pyOpenSSL>=0.10',
-        'django-fields>=0.3.1'
+        'django-fields-py3>=0.3.1'
     ],
     dependency_links=[
         'https://github.com/Octane5Devs/django-fields-py3/tarball/main#egg=django-fields-0.3.1',
