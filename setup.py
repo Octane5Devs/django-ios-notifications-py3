9878365a91a4c1c42b3ba3a356e26e0877db0106
Adam Pugliese <adampugliese@Adams-MBP.attlocal.net>
2021-11-15 15:06:37 -0500

    Package name change

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'Django>=1.3',
         'pyOpenSSL>=0.10',
-        'django-fields-py3'
+        'django-fields'
     ],
     dependency_links=[
         'https://github.com/Octane5Devs/django-fields-py3',
